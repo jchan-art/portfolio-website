b8fe5802974a8f34d2e743f0b324b5efaffdbb58
jchan <jchan@users.noreply.github.com>
2020-10-15 23:54:00 -0400

    fixed code errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hello! Welcome to my portfolio");
+console.log('Hello! Welcome to my portfolio')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
